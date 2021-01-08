a1f04dc94100f345b4a9a2f9ca191782722d3c2c
chopdawg-mario <thefozcat@gmail.com>
2021-01-08 14:46:29 -0800

    added and configured axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Import Components
 import Hero from "../components/Hero";
 
 export default function Home() {
+
+    const [heroData, setHeroData] = useState({});
+
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_API_URL}`)
+            .then(res => {
+                setHeroData(res.data);
+            })
+            .catch(err => {
+                alert('There was an error retrieving data')
+            })
+    });
+
     return (
         <div className="container flex items-center mx-auto justify-center">
-            <h1>Hi there!</h1>
+            <Hero src="/images/hero-example.png" />
         </div>
     )
 }
